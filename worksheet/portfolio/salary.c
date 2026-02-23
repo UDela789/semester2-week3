a2d503c61acd06a3775c7b3d0fb4a8316d8cee8d
Delalorm Kunutsor <gmbp0709@leeds.ac.uk>
2026-02-23 12:00:54 +0000

    adsfg

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -10,9 +10,9 @@
  int main( void ) {
 
     // define and initialise variables for the problem data 
-    int salary = 36250;
+    float salary = 36250;
     float NI = 0.08;
-    float tax_rate = 0.2;
+    float tax_rate = 0.15;
 
     // calculate the deductions and final take-home salary
     float NI_deduction = salary * NI;
@@ -20,10 +20,10 @@
     float take_away_salary = salary - NI_deduction - tax_deduction;
 
     // Use only these print statement with appropriate formatting and variable names
-    printf("Salary £%d",salary);
-    printf("\nNI contribution £%.2f",NI_deduction);
-    printf("\nTax contribution £%.2f",tax_deduction);
-    printf("\nTake home salary £%.2f\n",take_away_salary);
+    printf("Salary £%.2f\n",salary);
+    printf("NI contribution £%.2f\n",NI_deduction);
+    printf("Tax contribution £%.2f\n",tax_deduction);
+    printf("Take home salary £%.2f\n",take_away_salary);
 
 
     return 0;
